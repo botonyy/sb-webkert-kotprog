df65c0de2406db058a87fac7b37995d26a2a7007
Suge <suge.botond@gmail.com>
2022-05-05 11:22:06 +0200

    mukodo routing komponensek kozott

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+      path: '',
+      component: MainComponent,
+  },
+  {
+      path: 'contact',
+      component: ContactComponent,
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -18,10 +18,5 @@ export class MainNavComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
   
-  page = 'main';
-  
-  
-  
-  
-
 }
+
